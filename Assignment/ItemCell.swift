4b401fdf87bf759cdcad5f82194453078c30db22
Henry <rabbit.henry@gmail.com>
2016-06-06 18:28:16 +0800

    Implement infinite scrolling and api loading. Show indicator when loading. Implement layout of cell and bind data.

---
+++ Assignment/ItemCell.swift
@@ -0,0 +1,40 @@
+//
+//  ItemCell.swift
+//  Assignment
+//
+//  Created by Henry Tseng on 2016/6/6.
+//  Copyright © 2016年 EMQ. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet var senderLabel : UILabel!
+    @IBOutlet var recipientLabel : UILabel!
+    @IBOutlet var amountLabel : UILabel!
+    @IBOutlet var noteLabel : UILabel!
+    @IBOutlet var timeLabel : UILabel!
+    @IBOutlet var idLabel : UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setupLayout(item : Item) {
+        self.senderLabel.text = item.source.sender
+        self.recipientLabel.text = item.destination.recipient
+        self.amountLabel.text = "\(item.destination.currency) \(item.destination.amount)"
+        self.noteLabel.text = item.source.note
+        self.idLabel.text = "\(item.id)"
+    }
+
+}
--- Assignment/ListViewController.swift
+++ Assignment/ListViewController.swift
@@ -12,21 +12,106 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     @IBOutlet var tableView : UITableView!
     var items : [Item] = []
+    var range : Range<Int> = 0..<0
+    let loadingLock = NSLock()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ListManager.sharedManager.updateList(20, offset: 0) { (items) in
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 100
+        self.tableView.tableFooterView?.hidden = false
+        ListManager.sharedManager.updateList(30, offset: 0) { (items) in
             guard let items = items else {
                 return
             }
             self.items = items
+            self.range = 0..<self.items.count
             self.tableView.reloadData()
+            self.tableView.tableFooterView?.hidden = true
         }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func loadPreviousPage() {
+        if self.loadingLock.tryLock() {
+            self.tableView.tableHeaderView?.hidden = false
+            let offset = Int(self.range.startIndex) - 10
+            ListManager.sharedManager.updateList(10, offset: offset) { (items) in
+                guard let items = items else {
+                    return
+                }
+                self.items.removeRange(20..<30)
+                var deleteIndexPaths = [NSIndexPath]()
+                var insertIndexPaths = [NSIndexPath]()
+                
+                for i in 0..<10 {
+                    let indexPath = NSIndexPath(forRow: i, inSection: 0)
+                    insertIndexPaths.append(indexPath)
+                }
+                
+                for i in 20..<30 {
+                    let indexPath = NSIndexPath(forRow: i, inSection: 0)
+                    deleteIndexPaths.append(indexPath)
+                }
+                
+                for (idx, item) in items.enumerate() {
+                    self.items.insert(item, atIndex: idx)
+                }
+                self.range.startIndex -= 10
+                self.range.endIndex -= 10
+                self.tableView.beginUpdates()
+                self.tableView.deleteRowsAtIndexPaths(deleteIndexPaths, withRowAnimation: .None)
+                self.tableView.insertRowsAtIndexPaths(insertIndexPaths, withRowAnimation: .None)
+                self.tableView.endUpdates()
+                self.tableView.scrollToRowAtIndexPath(NSIndexPath(forRow: 10, inSection: 0), atScrollPosition: UITableViewScrollPosition.Top, animated: false)
+                self.tableView.tableHeaderView?.hidden = true
+                
+                self.loadingLock.unlock()
+            }
+        }
+    }
+    
+    private func loadNextPage() {
+        if self.loadingLock.tryLock() {
+            self.tableView.tableFooterView?.hidden = false
+            let offset = Int(self.range.endIndex)
+            ListManager.sharedManager.updateList(10, offset: offset) { (items) in
+                guard let items = items else {
+                    return
+                }
+                self.items.removeRange(0..<10)
+                var deleteIndexPaths = [NSIndexPath]()
+                var insertIndexPaths = [NSIndexPath]()
+                
+                for i in 0..<10 {
+                    let indexPath = NSIndexPath(forRow: i, inSection: 0)
+                    deleteIndexPaths.append(indexPath)
+                }
+                
+                for i in 20..<30 {
+                    let indexPath = NSIndexPath(forRow: i, inSection: 0)
+                    insertIndexPaths.append(indexPath)
+                }
+                
+                for item in items {
+                    self.items.append(item)
+                }
+                self.range.startIndex += 10
+                self.range.endIndex += 10
+                self.tableView.beginUpdates()
+                self.tableView.deleteRowsAtIndexPaths(deleteIndexPaths, withRowAnimation: .None)
+                self.tableView.insertRowsAtIndexPaths(insertIndexPaths, withRowAnimation: .None)
+                self.tableView.endUpdates()
+                
+                self.tableView.tableFooterView?.hidden = true
+                
+                self.loadingLock.unlock()
+            }
+        }
     }
 
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -39,6 +124,19 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        if let cell = cell as? ItemCell {
+            let item = self.items[indexPath.row]
+            cell.setupLayout(item)
+        }
+        
+        if indexPath.row < 3 && self.range.startIndex > 0 {
+            self.loadPreviousPage()
+        }
+        
+        if indexPath.row > 27 {
+            self.loadNextPage()
+        }
         
         return cell
     }
